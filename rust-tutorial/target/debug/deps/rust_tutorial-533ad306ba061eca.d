ea0300e043b3206db32069d6487f3837dc37f1a3
jerrymusaga <musagatechnology@gmail.com>
2023-04-27 17:50:09 +0100

    first commit

---
+++ rust-tutorial/target/debug/deps/rust_tutorial-533ad306ba061eca.d
@@ -0,0 +1,5 @@
+/home/musaga/wba/rust-tutorial/target/debug/deps/rust_tutorial-533ad306ba061eca.rmeta: src/main.rs
+
+/home/musaga/wba/rust-tutorial/target/debug/deps/rust_tutorial-533ad306ba061eca.d: src/main.rs
+
+src/main.rs:
---
+++ rust-tutorial/target/debug/deps/rust_tutorial-b99bb28e716263f3.d
@@ -0,0 +1,5 @@
+/home/musaga/wba/rust-tutorial/target/debug/deps/rust_tutorial-b99bb28e716263f3.rmeta: src/main.rs
+
+/home/musaga/wba/rust-tutorial/target/debug/deps/rust_tutorial-b99bb28e716263f3.d: src/main.rs
+
+src/main.rs:
